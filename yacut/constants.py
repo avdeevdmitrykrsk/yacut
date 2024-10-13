0aa165ccf6abb825ba34e039d484e24cd6471c9f
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2024-10-14 01:28:49 +0700

    pref

--- yacut/constants.py
+++ yacut/constants.py
@@ -1,10 +1,10 @@
 from string import ascii_letters, digits, punctuation, whitespace
 
-RUSSIAN_ALPHABET = (
-    'абвгдеёжзийклмнопрстуфхцчшщъыьэюя '
-    'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-)
+russian_uppercase = ''.join(chr(1040 + i) for i in range(32))
+russian_lowercase = ''.join(chr(1072 + i) for i in range(32))
 SHORT_ID_CHOICES = ascii_letters + digits
 DEFAULT_SHORT_ID_LENGTH = 6
 MAX_SHORT_ID_LENGTH = 16
-UNSUPPORTED_LETTERS = punctuation + RUSSIAN_ALPHABET + whitespace
+UNSUPPORTED_LETTERS = (
+    punctuation + russian_uppercase + russian_lowercase + whitespace + ' '
+)
